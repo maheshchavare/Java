09dee41a721eac0a28e6af70aed1b8d9b6c43d63
maheshchavare <maheshchavare@gmail.com>
2020-05-23 07:39:15 +0530

    Encapsulation demo

---
+++ JavaTutorial_Telusko/src/Encapsulation.java
@@ -0,0 +1,29 @@
+class Students {
+	private int rollNo;
+	private String name;
+	public int getRollNo() {
+		return rollNo;
+	}
+	public void setRollNo(int rollNo) {
+		this.rollNo = rollNo;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
+
+public class Encapsulation {
+	public static void main(String[] args) {
+		Students s1 = new Students();
+		s1.setRollNo(10);
+		s1.setName("Mahesh Chavare");
+		
+		System.out.println(s1.getName());
+		System.out.println(s1.getRollNo());
+		
+	}
+}
